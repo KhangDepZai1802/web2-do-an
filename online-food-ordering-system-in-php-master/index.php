85bd63acd92ef0733f51b218ce68cee84174a77a
kimkhanh2810 <kimkhanh28102004@gmail.com>
2024-05-06 00:03:19 +0700

    Khanh

--- online-food-ordering-system-in-php-master/index.php
+++ online-food-ordering-system-in-php-master/index.php
@@ -31,9 +31,7 @@ session_start(); //start temp session until logout/browser closed
             <nav class="navbar navbar-dark">
                 <div class="container">
                     <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#mainNavbarCollapse">&#9776;</button>
-                    <a class="navbar-brand" href="index.php"> <img class="img-rounded"  src="images/logo.png" alt="" width=40px height=40px>
-                
-                </a>
+                    <a class="navbar-brand" href="index.php"> <img class="img-rounded" src="images/logo.png" alt="" width=40px height=40px> </a>
                     <div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
                         <ul class="nav navbar-nav">
                             <li class="nav-item"> <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
@@ -253,7 +251,7 @@ session_start(); //start temp session until logout/browser closed
                     <div class="restaurant-listing">
                         
 						
-                    <?php  //fetching records from table and filter using html data-filter tag
+						<?php  //fetching records from table and filter using html data-filter tag
 						$ress= mysqli_query($db,"select * from restaurant");  
 									      while($rows=mysqli_fetch_array($ress))
 										  {
@@ -291,7 +289,11 @@ session_start(); //start temp session until logout/browser closed
 										  }
 						
 						
-						?>	
+						?>
+						
+							
+						
+					
                     </div>
                 </div>
                 <!-- restaurants listing ends -->
@@ -332,32 +334,30 @@ session_start(); //start temp session until logout/browser closed
                 <!-- top footer statrs -->
                 <div class="row top-footer">
                     <div class="col-xs-12 col-sm-3 footer-logo-block color-gray">
-                        <a href="#"> <img src="images/food-picky-logo.png" alt="Footer logo"> </a> <span>Order Delivery &amp; Take-Out </span> </div>
+                        <a href="#"> <img src="images/logo.png" alt="Footer logo" width=180px height=180px > </a> <span>Order Delivery &amp; Take-Out </span> </div>
                     <div class="col-xs-12 col-sm-2 about color-gray">
                         <h5>About Us</h5>
                         <ul>
                             <li><a href="#">About us</a> </li>
-                            <li><a href="#">History</a> </li>
-                            <li><a href="#">Our Team</a> </li>
-                            <li><a href="#">We are hiring</a> </li>
+                            
+                            
+                            
                         </ul>
                     </div>
                     <div class="col-xs-12 col-sm-2 how-it-works-links color-gray">
                         <h5>How it Works</h5>
                         <ul>
-                            <li><a href="#">Enter your location</a> </li>
-                            <li><a href="#">Choose restaurant</a> </li>
-                            <li><a href="#">Choose meal</a> </li>
-                            <li><a href="#">Pay via credit card</a> </li>
+                            
+                            <li><a href="#">Choose branch</a> </li>
                             <li><a href="#">Wait for delivery</a> </li>
                         </ul>
                     </div>
                     <div class="col-xs-12 col-sm-2 pages color-gray">
                         <h5>Pages</h5>
                         <ul>
-                            <li><a href="#">Search results page</a> </li>
-                            <li><a href="#">User Sing Up Page</a> </li>
-                            <li><a href="#">Pricing page</a> </li>
+                          
+                            
+                            
                             <li><a href="#">Make order</a> </li>
                             <li><a href="#">Add to cart</a> </li>
                         </ul>
@@ -365,16 +365,9 @@ session_start(); //start temp session until logout/browser closed
                     <div class="col-xs-12 col-sm-3 popular-locations color-gray">
                         <h5>Popular locations</h5>
                         <ul>
-                            <li><a href="#">Sarajevo</a> </li>
-                            <li><a href="#">Split</a> </li>
-                            <li><a href="#">Tuzla</a> </li>
-                            <li><a href="#">Sibenik</a> </li>
-                            <li><a href="#">Zagreb</a> </li>
-                            <li><a href="#">Brcko</a> </li>
-                            <li><a href="#">Beograd</a> </li>
-                            <li><a href="#">New York</a> </li>
-                            <li><a href="#">Gradacac</a> </li>
-                            <li><a href="#">Los Angeles</a> </li>
+                            <li><a href="#">District 1</a> </li>
+                            <li><a href="#">District 5</a> </li>
+                            <li><a href="#">District 3</a> </li>
                         </ul>
                     </div>
                 </div>
@@ -391,21 +384,17 @@ session_start(); //start temp session until logout/browser closed
                                 <li>
                                     <a href="#"> <img src="images/mastercard.png" alt="Mastercard"> </a>
                                 </li>
-                                <li>
-                                    <a href="#"> <img src="images/maestro.png" alt="Maestro"> </a>
-                                </li>
+                               
                                 <li>
                                     <a href="#"> <img src="images/stripe.png" alt="Stripe"> </a>
                                 </li>
-                                <li>
-                                    <a href="#"> <img src="images/bitcoin.png" alt="Bitcoin"> </a>
-                                </li>
+                                
                             </ul>
                         </div>
                         <div class="col-xs-12 col-sm-4 address color-gray">
                             <h5>Address</h5>
-                            <p>Concept design of oline food order and deliveye,planned as restaurant directory</p>
-                            <h5>Phone: <a href="tel:+080000012222">080 000012 222</a></h5> </div>
+                            <p>Concept design of oline food order and deliveye,planned as branch directory</p>
+                            <h5>Phone: <a href="tel:+080000012222">0909 683 737</a></h5> </div>
                         <div class="col-xs-12 col-sm-5 additional-info color-gray">
                             <h5>Addition informations</h5>
                             <p>Join the thousands of other restaurants who benefit from having their menus on TakeOff</p>
